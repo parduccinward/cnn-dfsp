47cce06abcb2330bae38a2b15041038bc3be2383
Diego <parduccinward@gmail.com>
2021-05-31 13:42:06 -0400

    aumentando data_augmentation.py

---
+++ src/model/classes.js
@@ -0,0 +1,7 @@
+// In this file we have all classes that the model can classify.
+export const MODEL_CLASSES = {
+    0: 'Lesion Benigna',
+    1: 'Carcinoma de Celulas Basales',
+    2: 'Dermatofibrosarcoma Protuberans',
+    3: 'Melanoma'
+};
