15d4a38506b72031176a162b3e93bc92129b2bc7
Diego <parduccinward@gmail.com>
2021-06-02 23:29:26 -0400

    preparando directorios para agregar streamlit

--- src/model/classes.js
+++
@@ -1,7 +0,0 @@
-// In this file we have all classes that the model can classify.
-export const MODEL_CLASSES = {
-    0: 'Lesion Benigna',
-    1: 'Carcinoma de Celulas Basales',
-    2: 'Dermatofibrosarcoma Protuberans',
-    3: 'Melanoma'
-};
